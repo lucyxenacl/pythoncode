d791ad19a807f0e73935e0f8ec8bced32955745a
Marisa Nickerson <mn8704@mcla.edu>
2018-01-25 00:53:29 +0000

    Updated ProgA1-2

--- ProgA1-2.py
+++ ProgA1-2.py
@@ -1,5 +1,6 @@
 #Programmed by Marisa Nickerson for Bioinformatics
 
-DNA = 'ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCTAGATG'
+print "Please enter DNA sequence"
+DNA = raw_input()
 print "The length of the first sequence is %s" % len(DNA[0:22])
 print "The Length of the second sequence is %s" % len(DNA[22:])
