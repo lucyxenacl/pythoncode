8b69eaae2b2992c9598ba5a0d71093b3bf5f7428
Marisa Nickerson <mn8704@mcla.edu>
2018-02-27 23:59:35 +0000

    updates

--- Prog2.py
+++ Prog2.py
@@ -1,6 +1,9 @@
+#Programmed by marisa nickerson for bioinformatics
 import bioinformatics
+# imports the function
 
 print bioinformatics.PctOfProtein("msrslllrfllfllllpplp", ["L"])
 print bioinformatics.PctOfProtein("MSRSLLLRFLLFLLLLPPLP", ["M"]) 
 print bioinformatics.PctOfProtein("MSRSLLLRFLLFLLLLPPLP", ['M', 'L']) 
 print bioinformatics.PctOfProtein("MSRSLLLRFLLFLLLLPPLP", ['F', 's', 'L']) 
+#prints the calculated percentage from the pieces of protein that were specified in the list
--- bioinformatics.py
+++ bioinformatics.py
@@ -1,3 +1,9 @@
+#programmed by marisa nickerson for bioinformatics
+# line 1 defines the function
+# line 2 changes the protein to uppercase so that the code isn't case sensitive
+# aa is amino acid and that is changed to upper from the list defined by codes
+# c is the count of codes within the protein 
+# the program should return the percentage of the list that was in the protein sequence
 def PctOfProtein(Protein, codes):
     Protein=Protein.upper()
     c=0 
